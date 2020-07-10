bee96773d8dcc24607821fec3870a780f2d72ce1
Harsh Solanki <hsolanki1884@gmail.com>
2020-07-11 04:13:44 +0530

    Updates

---
+++ precache-manifest.ad4ee10747ba28a260c76659100e65e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b05a00f598088d3768ee55ff7ab67d",
+    "url": "/poke-comporator/index.html"
+  },
+  {
+    "revision": "3d08c7e957dc52436bf5",
+    "url": "/poke-comporator/static/css/main.0c0416be.chunk.css"
+  },
+  {
+    "revision": "d19a3c97b13b10fc52b3",
+    "url": "/poke-comporator/static/js/2.4e57d179.chunk.js"
+  },
+  {
+    "revision": "d86f3a7a1e3c7f8afbd22066d3fc3c72",
+    "url": "/poke-comporator/static/js/2.4e57d179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d08c7e957dc52436bf5",
+    "url": "/poke-comporator/static/js/main.0e8f6650.chunk.js"
+  },
+  {
+    "revision": "e8f5ac63755e2493f07f",
+    "url": "/poke-comporator/static/js/runtime-main.2079f747.js"
+  }
+]);
